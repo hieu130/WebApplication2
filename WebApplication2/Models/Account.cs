7eeceb208ec8c2a4c2d0da3998ca7bb1bc99f33d
hieu130 <hieuvanphan275@gmail.com>
2022-04-29 08:52:44 +0700

    Add project files.

---
+++ WebApplication2/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime DisabledAt { get; set; }
+        public string Address { get; set; }
+        public string Thumbnail { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Status { get; set; }
+        public string RoleNumber { get; set; }
+        public string EmployeeNumber { get; set; }
+        public DateTime AddmissionDate { get; set; }
+        public DateTime DateOfJoining { get; set; }
+    }
+}
